1700150c8f55e9a98b37193ccc7ffe2f76721e17
jomorogit <jomoroforwork@gmail.com>
2026-06-01 21:50:42 +0200

    fix: prisma generate on build and store page

--- app/(shop)/store/page.tsx
+++ app/(shop)/store/page.tsx
@@ -137,6 +137,7 @@ export default async function Store(props: {
 
                 <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-4 gap-4 md:gap-8">
                     {games.length > 0 ? (
+                        // заменил типизацию map
                         games.map((game: typeof games[number]) => (
                             <CreateCard 
                                 key={game.id}
